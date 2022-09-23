e4901d85123b7683e278842fea73da257a8f70bd
cryo5551 <96884049+cryo5551@users.noreply.github.com>
2022-09-23 18:42:23 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ const Nav = () => {
             <img src={icon} alt="dp" id='icon' />
             <h3>EMOjee</h3>
             
-            <a href="https://github.com/" className='git' target="blank"><img src={logo} alt="gihub" id='gitlogo' /><p>Github</p></a>  
+            <a href="https://github.com/cryo5551/emojee-react" className='git' target="blank"><img src={logo} alt="gihub" id='gitlogo' /><p>Github</p></a>  
         </div>
     );
 }
